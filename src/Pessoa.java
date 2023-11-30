42821d2ae9e4982a25110a41fb90d69d589043c7
Daniel Martins <danielmartins888@gmail.com>
2023-11-30 17:19:53 -0300

    Adição das classes Item e Pessoa

---
+++ src/Pessoa.java
@@ -0,0 +1,20 @@
+public abstract class Pessoa {
+    protected String nome;
+    protected int idade;
+
+    public Pessoa(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    // Método abstrato para ser implementado pelas subclasses
+    public abstract void exibirDetalhes();
+}
